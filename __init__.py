04b675b33ed76a33f2c7940c2e956c35183eeaed
Tony Patton <aspatton@users.noreply.github.com>
2020-07-02 21:24:10 -0400

    Add files via upload
    First Azure function in Python - it tests IP address via regular expressions to see if valid.

---
+++ __init__.py
@@ -0,0 +1,48 @@
+import logging
+import re
+
+import azure.functions as func
+
+def validate_ip4_address(IP):
+    regexIP4 = '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$'
+    try:
+        progIP4 = re.compile(regexIP4)
+        resultIP4 = progIP4.match(IP)
+    except:
+        resultIP4 = None
+    return resultIP4 
+
+def validate_ip6_address(IP):
+    regexIP6 = '^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$'
+    try:
+        progIP6 = re.compile(regexIP6)
+        resultIP6 = progIP6.match(IP)
+    except:
+        resultIP6 = None
+    return resultIP6
+    
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        if (validate_ip4_address(name) != None):
+            val = '{} is a valid IP4 address'.format(name)
+        elif (validate_ip6_address(name) != None):
+            val = '{} is a valid IP6 address'.format(name)
+        else:
+            val = "Ooops, there was a problem."
+        return func.HttpResponse(val)
+    else:
+        return func.HttpResponse(
+             "Please pass a name on the query string or in the request body",
+             status_code=400
+        )
